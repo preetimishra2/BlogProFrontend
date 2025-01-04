799c4690be49b88a95b23fc16f7b82f93b89c2a7
preetimishra2 <preetimishra244@gmail.com>
2025-01-04 20:04:19 +0530

    changes for url.js

--- src/url.js
+++ src/url.js
@@ -1,2 +1,12 @@
-export const URL = "http://localhost:8000";
-export const IF = "http://localhost:8000/images/";
+// Check if the app is in production or development environment
+const isProduction = process.env.NODE_ENV === "production";
+
+// Set the backend URL based on the environment
+export const URL = isProduction 
+    ? "https://blogbackendpro-pp5a.onrender.com" // Production backend URL
+    : "http://localhost:8000"; // Local development backend URL
+
+// Set the image URL based on the environment
+export const IF = isProduction 
+    ? "https://blogbackendpro-pp5a.onrender.com/images/" // Production image URL
+    : "http://localhost:8000/images/"; // Local development image URL
