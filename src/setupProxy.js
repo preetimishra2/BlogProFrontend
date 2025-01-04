4494e0f2f9f2c791b89eb1d21023bc41eb27f175
preetimishra2 <preetimishra244@gmail.com>
2025-01-04 19:23:57 +0530

    setting up production

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,15 +1,25 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (root) {
+    const target =
+        process.env.NODE_ENV === 'production'
+            ? 'https://blogbackendpro-pp5a.onrender.com' // Deployed backend for production
+            : 'http://localhost:8000'; // Local backend for development
+
+    const allowedOrigin =
+        process.env.NODE_ENV === 'production'
+            ? 'https://blogprofrontend.onrender.com' // Deployed frontend URL
+            : 'http://localhost:3000'; // Local frontend URL
+
     root.use(
-    '/api', // Adjust the path you want to proxy
-    createProxyMiddleware({
-      target: 'http://localhost:8000', // Specify the address of your backend server
-      changeOrigin: true,
-      secure: false, // Set to false if your backend doesn't use HTTPS
-      headers: {
-        'Access-Control-Allow-Origin': 'http://localhost:3000', // Adjust the React app's origin
-      },
-    })
-  );
+        '/api', // Adjust the path you want to proxy
+        createProxyMiddleware({
+            target: target,
+            changeOrigin: true,
+            secure: process.env.NODE_ENV === 'production', // Secure only in production
+            headers: {
+                'Access-Control-Allow-Origin': allowedOrigin, // Adjust based on the environment
+            },
+        })
+    );
 };
